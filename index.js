a3a8439b15e722f428e3715fc37751717bd7572e
Emmanuel Ajibola <emiderex112@gmail.com>
2025-05-22 23:45:48 +0100

    JS

---
+++ index.js
@@ -0,0 +1,7 @@
+const accept = function(username, password){
+    if(userName === "Samuel" && password === "1234556"){
+        console.log("Welcome to your account");
+    }else{
+        console.log("Username or Password is incorrect");
+    }
+}
