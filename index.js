2ef61b5eb385fc086a0cae9d99901d8762094433
Emmanuel Ajibola <emiderex112@gmail.com>
2025-05-23 20:54:02 +0100

    Validation

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const accept = function(username, password){
-    if(userName === "Samuel" && password === "1234556"){
-        console.log("Welcome to your account");
-    }else{
-        console.log("Username or Password is incorrect");
+// This is a simple JavaScript program that prompts the user for their name and password
+//userName
+let userName = prompt("Enter your name: ");
+
+function usersName(){
+    if (usersName >= 10){
+     return prompt("Enter your name: ");   
+    } 
+}
+
+//userPassword
+let password = prompt("Enter your Password: ");
+
+function userPassword(){
+    if (password.length >= 10){
+        return prompt("Enter your Password: ");
     }
 }
