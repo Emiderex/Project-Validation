17c91fcfd1a8ac7eee5bd793d344498902a95f07
Emmanuel Ajibola <emiderex112@gmail.com>
2025-06-02 08:36:36 +0100

    Code update

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 
-
+//Declaring a variable & prompting for the username
 let username = prompt("Enter your name: ");
 validateUsername(username);
 
+//Re prompt the user for a valid input that meets the criteria set for a username
+while (validateUsername(username) === false){
+    username = prompt("Invalid username. Please enter a valid username containing 10 characters:")
+}
+
+//Decalaring the variable & prompting the user for password 
 let password = prompt("Enter your password:");
-console.log(validatePassword(password));
+
+//Re prompt the user for a valid input that meets the criteria set for the password
+while (validatePassword(password) === false){
+    password = prompt("Invalid password. try again. Password must be six characters long")
+}
+ 
+//Output the username and password
+console.log("Username: " + username + "\nPassword: " + password);
+console.log("You have successfully logged in!");
 
 
+//Function to validate the username - this check if the username is null, empty or greater than 10 characters
 function validateUsername(username){
     if(username === null) {
-        return false
+        return true
     }
 
     if(username === "") {
@@ -23,9 +38,10 @@ function validateUsername(username){
     }
 }
 
+//Function to validate the password - this checks if the password is null, empty or less than 6 characters
 function validatePassword(password) {
     if (password === null){
-        return false;
+        return true;
     }
 
     if (password === ""){
